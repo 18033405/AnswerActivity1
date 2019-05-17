3b7920db9278aba2a4af4597ae7edc12e530ff9d
18033405 <18033405@myrp.edu.sg>
2019-05-17 23:36:36 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/answeractivity1/MainActivity.java
@@ -0,0 +1,16 @@
+package sg.edu.rp.c346.answeractivity1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
